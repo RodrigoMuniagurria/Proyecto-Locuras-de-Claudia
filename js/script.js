bb70a0fac804917c285f79c88a02a408e5b33fa3
RodrigoMuniagurria <msrodrigo71@gmail.com>
2022-07-11 20:46:24 -0300

    javascript

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let nombrePersona = prompt("Bienvenido ingrese su nombre:")
+
+alert("Bienvenido " + nombrePersona);
+
+let numero = parseInt(prompt("¿De cuantos integrantes es tu familia?"))
+
+for(i = 1 ; i <= numero ; i++){
+    alert("Felicidades, los integrantes de tu familia son: " + i)
+}
+
+
